0217e3d091a8360383daa66856ffa88018de5efd
afyifah <mailwork.afifah@gmail.com>
2024-01-10 00:49:25 +0700

    initial commit

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,10 @@
+import { Uikit } from'@nxsetup/uikit';
+
+export default async function Index() {
+  return (
+    <div>
+    <h1>INI ADALAH LANDING PAGE</h1>
+     <Uikit/>
+     </div>
+  );
+}
---
+++ libs/uikit/src/lib/uikit.tsx
@@ -0,0 +1,14 @@
+
+
+/* eslint-disable-next-line */
+export interface UikitProps {}
+
+export function Uikit(props: UikitProps) {
+  return (
+    <div style={{background:'red'}}>
+      <h1>Welcome to Uikit!</h1>
+    </div>
+  );
+}
+
+export default Uikit;
